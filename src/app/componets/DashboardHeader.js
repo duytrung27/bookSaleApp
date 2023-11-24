8385c2febdc5ec56087f8414f35eaa3ee8b94351
DESKTOP-VPI4O3L\ITSJ-Group <trungnd@hikesiya.co.jp>
2023-11-24 23:56:22 +0700

    added header animated

--- src/app/componets/DashboardHeader.js
+++ src/app/componets/DashboardHeader.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { CiSearch } from "react-icons/ci";
+import RevealOnScroll from "./RevealOnScroll";
 
 function DashboardHeader() {
   return (
@@ -15,26 +16,28 @@ function DashboardHeader() {
         />
       </div>
       <div className="container h-full w-full flex flex-col items-center lg:items-start justify-center text-white md:text-black">
-        <h1 className="z-10 leading-snug uppercase font-extrabold text-5xl sm:text-6xl">
-          Read and add <br /> your insight
-        </h1>
-        <h2 className="z-10 capitalize text-center text-[16px] sm:text-xl mt-3">
-          Find your favorite book and read it here for free
-        </h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log("object");
-          }}
-          className="z-10 flex items-center mt-5 rounded-[4px] shadow-md bg-neutral-100 max-w-[400px]"
-        >
-          <CiSearch color="black" size={20} className="ml-3" />
-          <input
-            type="text"
-            placeholder="Search Book"
-            className="w-full text-black bg-transparent border-none focus:ring-transparent"
-          />
-        </form>
+        <RevealOnScroll animate="z-10 animate__fadeInLeft">
+          <h1 className="z-10 !leading-snug uppercase font-extrabold text-5xl sm:text-6xl">
+            Read and add <br /> your insight
+          </h1>
+          <h2 className="z-10 capitalize text-center text-[16px] sm:text-xl mt-3">
+            Find your favorite book and read it here for free
+          </h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log("object");
+            }}
+            className="z-10 flex items-center mt-5 rounded-[4px] shadow-md bg-neutral-100 max-w-[400px]"
+          >
+            <CiSearch color="black" size={20} className="ml-3" />
+            <input
+              type="text"
+              placeholder="Search Book"
+              className="w-full text-black bg-transparent border-none focus:ring-transparent"
+            />
+          </form>
+        </RevealOnScroll>
       </div>
     </div>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import "animate.css";
 import { ReduxProvider } from "./utils/provider";
 import Nav from "./componets/Nav";
 
